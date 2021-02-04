4db42949224dac67e7876897dbe1b4168cb5d66b
andrewgremlich <andrew.gremlich@gmail.com>
2021-02-03 23:54:22 -0700

    First draft release.

---
+++ optimizeImages.sh
@@ -0,0 +1,9 @@
+# jpegoptim --strip-all --verbose --size=60% --max=70 --totals ./public/images/andrew_patriarchs.JPG
+# jpegoptim --strip-all --verbose --size=70% --max=30 --totals ./public/images/walk_in_the_park.jpg
+# jpegoptim --strip-all --verbose --size=70% --max=30 --totals ./public/images/hole_in_the_wall.jpg
+# jpegoptim --strip-all --verbose --size=70% --max=30 --totals ./public/images/zion_observation.jpg
+# jpegoptim --strip-all --verbose --size=70% --max=80 --totals ./public/images/christmas_tree.JPG
+# jpegoptim --strip-all --verbose --size=70% --max=30 --totals ./public/images/dino_hunt.jpg
+# jpegoptim --strip-all --verbose --size=70% --max=30 --totals ./public/images/surgery.JPG
+# jpegoptim --strip-all --verbose --size=70% --max=30 --totals ./public/images/bad_news_broken.JPG
+# jpegoptim --strip-all --verbose --size=70% --max=60 --totals ./public/images/metal_texture.jpg
