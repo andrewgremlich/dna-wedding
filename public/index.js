8cb15dead1f7bbfdfe6d1b3a2400100ccc4f1037
andrewgremlich <andrew.gremlich@gmail.com>
2021-02-17 23:30:39 -0700

    Timeline widget available

--- public/index.js
+++ public/index.js
@@ -7,6 +7,9 @@ if ("serviceWorker" in navigator) {
 const tableOfContentsClick = new Query("#tocClick");
 const tableOfContents = new Query("#toc");
 const exitTableOfContents = new Query("#tocExit");
+const leftNavigation = new Query("#navigateLeft");
+const rightNavigation = new Query("#navigateRight");
+const timelineViewport = new Query("#timelineViewport");
 
 const tocSlideInSlideOut = () => {
   const isOpen = tableOfContents.element.classList.contains("toc-slide-in");
@@ -20,3 +23,41 @@ const tocSlideInSlideOut = () => {
 tableOfContentsClick.element.addEventListener("click", tocSlideInSlideOut);
 
 exitTableOfContents.element.addEventListener("click", tocSlideInSlideOut);
+
+// TODO: Take into account the scroll position
+
+const style = window.getComputedStyle(timelineViewport.element);
+const width = style.width.replace("px", "");
+const widthAsNumber = Math.floor(width);
+let timelineScrollPosition = 0;
+
+leftNavigation.element.addEventListener("click", () => {
+  const newScrollPosition = timelineScrollPosition;
+
+  if (newScrollPosition > 0) {
+    timelineScrollPosition -= widthAsNumber;
+
+    timelineViewport.element.scroll({
+      top: 0,
+      left: timelineScrollPosition,
+      behavior: "smooth",
+    });
+  }
+});
+
+rightNavigation.element.addEventListener("click", () => {
+  const newScrollPosition = timelineScrollPosition + widthAsNumber;
+
+  if (
+    timelineViewport.element.scrollWidth - widthAsNumber >
+    newScrollPosition
+  ) {
+    timelineScrollPosition = newScrollPosition;
+
+    timelineViewport.element.scroll({
+      top: 0,
+      left: timelineScrollPosition,
+      behavior: "smooth",
+    });
+  }
+});
